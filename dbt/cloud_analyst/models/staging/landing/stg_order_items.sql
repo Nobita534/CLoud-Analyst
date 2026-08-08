008da66575a7b351ebd398d05a0bc69d96d4033a
Phuc <10a1tranhongphuc2021@gmail.com>
2026-08-08 22:55:13 +0700

    feat: set up dbt-core and complete staging layer

---
+++ dbt/cloud_analyst/models/staging/landing/stg_order_items.sql
@@ -0,0 +1,16 @@
+with source as (
+
+    select *
+    from {{ source('olist', 'olist_order_items_dataset') }}
+
+)
+
+select
+    order_id,
+    order_item_id,
+    product_id,
+    seller_id,
+    shipping_limit_date,
+    price,
+    freight_value
+from source
