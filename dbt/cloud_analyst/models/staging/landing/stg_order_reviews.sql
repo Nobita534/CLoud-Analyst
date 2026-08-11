aa5fc0dc004cae4d17de02b261902f5bf08c29fd
Phuc <10a1tranhongphuc2021@gmail.com>
2026-08-11 21:44:52 +0700

    fix: handle review_score null cases

--- dbt/cloud_analyst/models/staging/landing/stg_order_reviews.sql
+++ dbt/cloud_analyst/models/staging/landing/stg_order_reviews.sql
@@ -2,7 +2,7 @@ with source as (
 
     select *
     from {{ source('olist', 'olist_order_reviews_dataset') }}
-    where order_id is not null
+    where order_id is not null and review_score is not null
 
 )
 
