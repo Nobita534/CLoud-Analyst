920bfe2d7b8ed5b971e49aad01ba9ff136709901
Phuc <10a1tranhongphuc2021@gmail.com>
2026-08-11 21:46:34 +0700

    fix: handle product_name null cases by fill Unknown

--- dbt/cloud_analyst/models/intermediate/int_products.sql
+++ dbt/cloud_analyst/models/intermediate/int_products.sql
@@ -1,7 +1,7 @@
 WITH staging_product_data AS(
     SELECT 
         product_id,
-        product_category_name
+        COALESCE(product_category_name, 'Unknown') AS product_category_name
     FROM {{ref("stg_products")}}
 )
 
