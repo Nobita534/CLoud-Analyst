41cedf9c12a4958aa7249676260682961eef16b6
Phuc <10a1tranhongphuc2021@gmail.com>
2026-08-12 19:49:42 +0700

    fix: handle duplicate rows

--- dbt/cloud_analyst/models/marts/dimensions/dim_customers.sql
+++ dbt/cloud_analyst/models/marts/dimensions/dim_customers.sql
@@ -1,21 +1,31 @@
-WITH int_customer_data AS (
+WITH int_geolocation_data AS (
 
     SELECT
-        customer_unique_id,
-        customer_id,
-        customer_zip_code_prefix,
-        customer_city,
-        customer_state
+        geolocation_zip_code_prefix,
+        MAX(geolocation_city) AS geolocation_city,
+        MAX(geolocation_state) AS geolocation_state
 
-    FROM {{ ref('int_customer') }}
+    FROM {{ ref('int_geolocation') }}
+
+    GROUP BY geolocation_zip_code_prefix
+
+),
+
+int_customer_data AS (
+
+    SELECT
+        c.customer_unique_id,
+        c.customer_id,
+        c.customer_zip_code_prefix,
+        c.customer_city,
+        c.customer_state
+
+    FROM {{ ref('int_customer') }} AS c
+
+    LEFT JOIN int_geolocation_data AS g
+        ON c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
 
 )
 
-SELECT
-    customer_unique_id,
-    customer_id,
-    customer_zip_code_prefix,
-    customer_city,
-    customer_state
-
+SELECT *
 FROM int_customer_data
