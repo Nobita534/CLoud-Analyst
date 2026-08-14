2aa942616b0e78a130a336e2caf09e94300ba229
Phuc <10a1tranhongphuc2021@gmail.com>
2026-08-14 22:35:56 +0700

    fix(metric-layer): add keys to make relationships in customer_count, order and revenue metrics

--- dbt/cloud_analyst/models/marts/metric_layer/sales/order_metrics.sql
+++ dbt/cloud_analyst/models/marts/metric_layer/sales/order_metrics.sql
@@ -1,4 +1,15 @@
 SELECT
-    COUNT(order_id) AS total_orders
-FROM {{ref('dim_orders')}}
+    o.customer_id,
+    oi.seller_id,
+    oi.product_id,
+    order_delivered_customer_date_key,
+    COUNT(DISTINCT o.order_id) AS total_orders
+FROM {{ref('dim_orders')}} o
+JOIN {{ref('dim_customers')}} c ON o.customer_id = c.customer_id
+JOIN {{ref('fact_order_items')}} oi ON oi.order_id = o.order_id
 WHERE order_status = 'delivered'
+GROUP BY
+    o.customer_id,
+    oi.seller_id,
+    oi.product_id,
+    order_delivered_customer_date_key
